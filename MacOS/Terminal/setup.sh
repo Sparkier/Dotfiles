53d60898f7b68c589745275ef6bbd22f0b721046
Alex <alex.baeuerle@uni-ulm.de>
2018-07-22 10:39:29 +0200

    Remove Strap specific commands..

--- MacOS/Terminal/setup.sh
+++ MacOS/Terminal/setup.sh
@@ -5,7 +5,7 @@ chsh -s /bin/zsh
 if ! [ -f "/Library/Developer/CommandLineTools/usr/bin/git" ] || \
    ! [ -f "/usr/include/iconv.h" ]
 then
-  log "Installing the Xcode Command Line Tools:"
+  echo "Installing the Xcode Command Line Tools:"
   CLT_PLACEHOLDER="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
   sudo touch "$CLT_PLACEHOLDER"
   CLT_PACKAGE=$(softwareupdate -l | \
@@ -14,28 +14,18 @@ then
   sudo softwareupdate -i "$CLT_PACKAGE"
   sudo rm -f "$CLT_PLACEHOLDER"
   if ! [ -f "/usr/include/iconv.h" ]; then
-    if [ -n "$STRAP_INTERACTIVE" ]; then
-      echo
-      logn "Requesting user install of Xcode Command Line Tools:"
-      xcode-select --install
-    else
-      echo
-      abort "Run 'xcode-select --install' to install the Xcode Command Line Tools."
-    fi
+    echo "Requesting user install of Xcode Command Line Tools:"
+    xcode-select --install
   fi
-  logk
+  echo "OK"
 fi
 
 # Check if the Xcode license is agreed to and agree if not.
 xcode_license() {
   if /usr/bin/xcrun clang 2>&1 | grep $Q license; then
-    if [ -n "$STRAP_INTERACTIVE" ]; then
-      logn "Asking for Xcode license confirmation:"
-      sudo xcodebuild -license
-      logk
-    else
-      abort "Run 'sudo xcodebuild -license' to agree to the Xcode license."
-    fi
+    echo "Asking for Xcode license confirmation:"
+    sudo xcodebuild -license
+    echo "OK"
   fi
 }
 xcode_license
