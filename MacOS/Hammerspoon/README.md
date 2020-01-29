785a5b568497b58f3fbc82fdf5c2d60ca0333d06
Alex <alex.baeuerle@uni-ulm.de>
2020-01-29 18:37:43 +0100

    Hammerspoon Added
    Added the Hammerspoon config to this repo.

---
+++ MacOS/Hammerspoon/README.md
@@ -0,0 +1,4 @@
+# Hammerspoon
+ Hammerspoon is an application that can automate tasks with MacOs. For me, it works with Karabiner to extend the
+ functionality of what you can do with keyboard shortcuts.
+ This contains the configuration for Hammerspoon.
---
+++ MacOS/Hammerspoon/init.lua
@@ -0,0 +1,26 @@
+-- Create the hyper alias
+local hyper = { "cmd", "alt", "ctrl", "shift" }
+
+-- Reload the config for hammerspoon
+hs.hotkey.bind(hyper, "0", function()
+  hs.reload()
+  hs.notify.new({title="Hammerspoon", informativeText="Config loaded"}):send()
+end)
+
+-- Bind keys to focus applications
+hs.hotkey.bind(hyper, "1", function()
+  hs.application.launchOrFocus('XCode')
+end)
+hs.hotkey.bind(hyper, "2", function()
+  hs.application.launchOrFocus('Safari')
+end)
+hs.hotkey.bind(hyper, "3", function()
+  hs.application.launchOrFocus('iTerm')
+end)
+hs.hotkey.bind(hyper, "4", function()
+  hs.application.launchOrFocus('WhatsApp')
+end)
+hs.hotkey.bind(hyper, "5", function()
+  hs.application.launchOrFocus('Mail')
+end)
+
